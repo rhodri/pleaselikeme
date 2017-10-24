8cb8c4dc0bacc7186266b8003479f51f96ae98f3
Rhodri Karim <rhodri@gmail.com>
2017-10-24 22:58:43 +0100

    Adding event handler.

--- public/javascripts/tinder.js
+++ public/javascripts/tinder.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
   $('.swiper').slick({
     arrows: false
+  }).on('swipe', function (e, slick, direction) {
+    console.log($('.swiper').slick('slickCurrentSlide'));
+    console.log(direction);
   });
-
-
 });

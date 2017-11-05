23d99e4a4d1892c1b34fef25dd10c9c4b7734158
Rhodri Karim <rhodri@gmail.com>
2017-11-05 14:52:09 +0000

    Add intro slide and update slide on swipe.

--- public/javascripts/app/swiper.js
+++ public/javascripts/app/swiper.js
@@ -5,24 +5,49 @@ define(["jquery", "slick", "knockout"], function ($, slick, ko) {
 
   function onSwipe(e, slick, direction) {
     console.log(direction);
-    if (elem.slick('slickCurrentSlide') === 0) return;
+    // TODO: record swipe
   }
 
-  function setupElem(cssSelector) {
+  function afterChange(updateFn) {
+    return function(e, slick, slideIndex) {
+      if (elem.slick('slickCurrentSlide') === 0) return;
+      elem.hide();
+      updateFn();
+      elem.slick('slickGoTo', 0, true);
+      elem.fadeIn(500);
+    }
+  }
 
+  var afterIntro = afterChange(function () {
+    model.intro(false);
+    elem.off('afterChange', afterIntro);
+    elem.on('swipe', onSwipe);
+    elem.on('afterChange', afterPortrait);
+  });
+
+  var afterPortrait = afterChange(function () {
+    model.idx(model.idx() + 1);
+  });
+
+  function slickElem(cssSelector) {
     var elem = $(cssSelector);
     elem.slick({
       arrows: false,
       waitForAnimate: false // to allow slickGoTo on swipe
-    }).on('swipe', onSwipe);
-
+    });
     return elem;
   }
 
   function createModel() {
 
     var self = {};
-    self.name = ko.observable(names[0]);
+
+    self.intro = ko.observable(true);
+    self.idx = ko.observable(0);
+
+    self.name = ko.computed(function () {
+      return names[self.idx()];
+    })
     self.imgUrl = ko.computed(function () {
       return '/images/' + self.name() + '.jpg';
     });
@@ -32,9 +57,12 @@ define(["jquery", "slick", "knockout"], function ($, slick, ko) {
 
   return {
     init: function(cssSelector) {
-      elem = setupElem(cssSelector);
+
       model = createModel();
       ko.applyBindings(model);
+
+      elem = slickElem(cssSelector);
+      elem.on('afterChange', afterIntro);
     }
   }
 });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,7 @@ requirejs.config({
   "baseUrl": "javascripts/lib",
   "paths": {
     "app": "../app",
-    "jquery": "https://code.jquery.com/jquery-3.2.1.slim.min",
+    "jquery": "https://code.jquery.com/jquery-3.2.1.min",
     "popper": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min",
     "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min",
     "knockout": "https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min"
